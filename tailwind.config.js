aa489410ff23a2ee378d2f17659b8f60148dc3d2
Umutonipamera <umutonipamera@gmail.com>
2021-10-04 08:35:05 +0300

    TailwindProject

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      backgroundImage:{
+        "custom-background-image":"url"("/images/background.jpg"),
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
